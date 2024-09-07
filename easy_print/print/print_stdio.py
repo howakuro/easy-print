5db385c1fc6482972a0911a559dfd9ad5ec591ff
howakuro <kaokata@msn.com>
2024-09-07 16:10:38 +0900

    feature: 間違ったコメントの作成

--- easy_print/print/print_stdio.py
+++ easy_print/print/print_stdio.py
@@ -5,18 +5,6 @@ from pathlib import Path
 class PrintStdIO(Print):
     def print(self, message: str, image=None):
         """
-        標準出力にメッセージを出力します
-
-        Parameters
-        ----------
-        message : str
-            出力するメッセージ
-        image : Path
-            画像を出力するにはこのパスを指定してください
-            ただし、標準入出力は画像出力に対応していません
-        """
-
-        """
         NOTE: 標準入出力は画像出力に対応していません
         """
         print(message)
